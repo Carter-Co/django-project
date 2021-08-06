ab64fd7b5e8127b7284bc89d46a65c9ef48932b4
Carter-Co <84072703+Carter-Co@users.noreply.github.com>
2021-08-06 15:17:52 +0800

    news - users

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, ProfileView 
 
 app_name = 'users'
 
 urlpatterns = [ 
-    path('create-account/', CreateAccountView.as_view(),
-    name='createAccount'),
+    path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('<int:pk>/',ProfileView.as_view(), name='profile'),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -9,3 +9,8 @@ class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class ProfileView(generic.DetailView):
+    model = CustomUser
+    template_name = 'users/profile.html'
+    context_object_name = 'user'
